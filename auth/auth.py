1d1c25ae063b553808ecbf42cfe645e4f291232f
Ilya Sereda <ilya.sereda2002@mail.ru>
2021-04-18 20:52:43 +0300

    Refactoring

--- auth/auth.py
+++ auth/auth.py
@@ -1,16 +1,16 @@
 from datetime import datetime, timedelta
 from typing import Optional
 
-from fastapi import Depends, FastAPI, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 
 from passlib.context import CryptContext
 
 from sqlalchemy.orm import Session
 from app.crud import crudUsers
-from app import schemas, models
-from app.database import SessionLocal, engine
+from app import schemas
+from app.database import SessionLocal
 
 SECRET_KEY = "be34e0f9c775b9c5f98aa37f2e6e70f851320d3ab1cfd186965ca7d0a61bcc0f"
 ALGORITHM = "HS256"
@@ -19,10 +19,6 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-# models.Base.metadata.create_all(bind=engine)
-
-# app1 = FastApi()
 
 
 def get_db():
@@ -85,29 +81,3 @@ async def get_current_active_user(current_user: schemas.UserBase = Depends(get_c
     if current_user.disabled:
         raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
-
-# @app1.post("/token", response_model=schemas.Token)
-# async def login_for_access_token(db: Session = Depends(get_db), form_data: OAuth2PasswordRequestForm = Depends()):
-#     user = auth.authenticate_user(db, form_data.username, form_data.password)
-#     if not user:
-#         raise HTTPException(
-#             status_code=status.HTTP_401_UNAUTHORIZED,
-#             detail="Incorrect username or password",
-#             headers={"WWW-Authenticate": "Bearer"},
-#         )
-#     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-#     access_token = auth.create_access_token(
-#         data={"sub": user.email}, expires_delta=access_token_expires
-#     )
-#     return {"access_token": access_token, "token_type": "bearer"}
-#
-#
-# @app1.get("/api/culture/users/me/", response_model=schemas.UserBase)
-# async def read_users_me(current_user: schemas.User = Depends(get_current_active_user)):
-#     return current_user
-#
-#
-# @app1.get("/api/culture/users/me/items/")
-# async def read_own_items(current_user: schemas.User = Depends(get_current_active_user)):
-#     return [{"user_student_ticket": current_user.student_ticket, "email": current_user.email}]
-#
